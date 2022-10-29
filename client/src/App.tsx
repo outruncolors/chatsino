1840fd049ac6d0129a79ec8406ea6fd0fc743fc9
Outrun Colors <itsoutruncolors@gmail.com>
2022-10-29 15:23:28 -0500

    Set up SSH for server and client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
 
-function App() {
+const SERVER_ADDRESS = "wss://localhost:8080";
+
+function useSocket() {
+  const socket = useRef<null | WebSocket>(null);
+
+  useEffect(() => {
+    if (!socket.current) {
+      socket.current = new WebSocket(SERVER_ADDRESS);
+
+      socket.current.onopen = function handleSocketOpen(event) {
+        console.log("Opened connection.", event);
+      };
+
+      socket.current.onclose = function handleSocketClose(event) {
+        console.log("Closed connection.", event);
+      };
+
+      socket.current.onerror = function handleSocketError(event) {
+        console.log("Encountered error.", event);
+      };
+
+      socket.current.onmessage = function handleSocketMessage(event) {
+        console.log("Received message.", event);
+      };
+    }
+  });
+
+  return socket.current;
+}
+
+export function App() {
+  useSocket();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -22,5 +51,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Henlo");
+import { createServer } from "https";
+import { readFileSync } from "fs";
+import path from "path";
+import { WebSocketServer } from "ws";
+
+const PORT = 9001;
+
+const server = createServer({
+  cert: readFileSync(path.join(__dirname, "../.ssh/localhost.pem")),
+  key: readFileSync(path.join(__dirname, "../.ssh/localhost-key.pem")),
+});
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+
+  ws.send("something");
+});
+
+console.log("Server listening on :%s", 8080);
+server.listen(8080);
